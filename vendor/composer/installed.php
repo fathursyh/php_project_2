acf2ef5a0f178510e6b5859dfe0b09271845ad93
fathursyh <fathur.syah1@gmail.com>
2024-03-28 23:19:52 +0700

    pc update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'pphobia-hp/php_projekkkk',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2ccea40277994a4bb8f93021967ee81dbcbe8656',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'pphobia-hp/php_projekkkk' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2ccea40277994a4bb8f93021967ee81dbcbe8656',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
