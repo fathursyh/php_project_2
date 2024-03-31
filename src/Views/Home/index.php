4bb8492cd9c805fb554247a501833c2102648ec6
fathursyh <fathur.syah1@gmail.com>
2024-03-31 21:01:41 +0700

    styling 1.4

--- src/Views/Home/index.php
+++ src/Views/Home/index.php
@@ -1,9 +1,22 @@
 <!DOCTYPE html>
 
 <main>
-<section class="utama" data-bs-toggle="">
-  <div class="container">
-    <div class="row"></div>
+<section class="utama">
+  <div class="p-5 text-center bg-body-tertiary rounded-3 h-100">
+    <svg class="bi mt-4 mb-3" style="color: var(--bs-indigo);" width="100" height="100"><use xlink:href="#bootstrap"></use></svg>
+    <h1 class="text-body-emphasis">Customer Centered</h1>
+    <p class="col-lg-8 mx-auto fs-5 text-muted">
+      This is a custom jumbotron featuring an SVG image at the top, some longer text that wraps early thanks to a responsive <code>.col-*</code> class, and a customized call to action.
+    </p>
+    <div class="d-inline-flex gap-2 mb-5">
+      <button class="d-inline-flex align-items-center btn btn-primary btn-lg px-4 rounded" type="button">
+        Call to action
+        <svg class="bi ms-2" width="24" height="24"><use xlink:href="#arrow-right-short"></use></svg>
+      </button>
+      <button class="btn btn-outline-secondary btn-lg px-4 rounded" type="button">
+        Secondary link
+      </button>
+    </div>
   </div>
 </section>
 </main>
--- src/Views/template/headers.php
+++ src/Views/template/headers.php
@@ -18,12 +18,12 @@
 <body>
 <nav class="navbar navbar-expand-lg bg-body-tertiary px-4">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand ms-3" href="#">DBZ</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse py-3" id="navbarNavDropdown">
-      <ul class="navbar-nav text-end">
+      <ul class="navbar-nav text-end ms-auto">
         <li class="nav-item rounded">
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
@@ -34,19 +34,16 @@
           <a class="nav-link" href="#">Pricing</a>
         </li>
         <li class="nav-item dropdown rounded">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="true" >
             Divisions
           </a>
-          <ul class="dropdown-menu">
+          <ul class="dropdown-menu dropdown-menu-end text-end">
             <li><a class="dropdown-item" href="#">Fathur Syah</a></li>
             <li><a class="dropdown-item" href="#">Gabriel Sebastian</a></li>
             <li><a class="dropdown-item" href="#">Muhammad Abian</a></li>
           </ul>
         </li>
       </ul>
-      <div class="ms-auto pe-4 my-0">
-        <img src=<?= BASEPATH . '/php_project_2/assets/user.png'?> alt="" width=40>
-      </div>
     </div>
   </div>
 </nav>
